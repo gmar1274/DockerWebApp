153362d5d488795375e45417b378d0fe839dbcff
Gabriel Martinez <noreply@cpp.edu>
2021-04-14 15:37:00 -0700

    Merge from spinner overlay branch

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
+const mysql = require('mysql');
+const events = require('./events');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password123',
+  database: 'myDB'
+});
+
+connection.connect();
+
+const app = express().use(cors()).use(events(connection));
 const port = process.env.PORT || 3080;
 const path = require('path');
 const users = [];
 
-app.use(express.static(path.join(__dirname,"dist")));
+app.use(express.static(path.join(__dirname, "dist")));
 //app.use('/view-users');
 
 app.get('/api/users', (req, res) => {
@@ -17,10 +30,10 @@ app.post('/api/user', (req, res) => {
   res.json("user addedd");
 });
 
-app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname,'dist','angular-ui','index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'angular-ui', 'index.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Server listening on the port::${port}`);
+  console.log(`Server listening on the port::${port}`);
 });
--- src/app/form/ol-form.component.ts
+++ src/app/form/ol-form.component.ts
@@ -10,17 +10,27 @@ import { HttpClient } from '@angular/common/http';
 })
 
 export class OlFormComponent {
-
+    
     @ViewChild('olForm', { static: true }) ngForm: NgForm;
 
     user = new User('', '', '', '', '', '', '', 0, 0);
     submitted = false;
+    error = false;
     onSubmit(data) {
         this.submitted = true;
-        console.log("Submit pressed!! ");
-        console.log(data);
+        //console.log("Submit pressed!! ");
+        //console.log(data);
         this.http.get("http://api.positionstack.com/v1/forward?access_key=d978d8c37e21cc1e2b44197ab283e307&query=" + data.address).subscribe(resp => {
-            console.log(resp);
+            let jsonstr = JSON.stringify(resp);
+            let json = JSON.parse(jsonstr);
+            console.log(json['data']);
+            if(json['data'].length==0){
+                console.log("Error");
+                this.error=true;
+            }else{
+                this.error=false;
+            }
+            this.ngForm.resetForm();
         });
     }
     ngOnInit() {
