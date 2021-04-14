2b4aec348940b725b762334a1200f88806cb4eb3
Gabriel Martinez <noreply@cpp.edu>
2021-04-13 23:45:22 -0700

    Geocoding implemented

--- src/app/form/ol-form.component.ts
+++ src/app/form/ol-form.component.ts
@@ -1,5 +1,7 @@
-import {Component} from '@angular/core';
-import {User} from './user';
+import { Component, ViewChild } from '@angular/core';
+import { User } from './user';
+import { NgForm } from '@angular/forms'
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: 'app-ol-form',
@@ -7,11 +9,24 @@ import {User} from './user';
     styleUrls: ['./ol-form.component.css']
 })
 
-export class OlFormComponent{
-    model = new User('test','','','','','','',0,0);
-    submitted=false;
-    onSubmit(){
-        this.submitted = true;
-    }
-}
+export class OlFormComponent {
 
+    @ViewChild('olForm', { static: true }) ngForm: NgForm;
+
+    user = new User('', '', '', '', '', '', '', 0, 0);
+    submitted = false;
+    onSubmit(data) {
+        this.submitted = true;
+        console.log("Submit pressed!! ");
+        console.log(data);
+        this.http.get("http://api.positionstack.com/v1/forward?access_key=d978d8c37e21cc1e2b44197ab283e307&query=" + data.address).subscribe(resp => {
+            console.log(resp);
+        });
+    }
+    ngOnInit() {
+        this.ngForm.form.valueChanges.subscribe(x => {
+            console.log(x);
+        })
+    }
+    constructor(private http: HttpClient) { }
+}
